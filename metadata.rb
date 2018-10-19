ef35e6387bc5c64c4dd0eb42f46758f4d5cc50bd
Jordan Fineberg <jordan@wherefor.com>
2018-10-19 14:44:02 -0700

    nodejs cookbook 6.0.0

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'Apache-2.0'
 description 'Install a verdaccio NPM server (cache & private repo)
 See : https://github.com/verdaccio/verdaccio/'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.0.2'
+version '2.0.3'
 source_url 'https://github.com/verdaccio/verdaccio-cookbook'
 issues_url 'https://github.com/verdaccio/verdaccio-cookbook/issues'
 chef_version '>= 12'
@@ -17,6 +17,6 @@ supports 'fedora'
 supports 'amazon'
 
 depends 'apt'
-depends 'nodejs', '~> 5.0.0'
+depends 'nodejs', '~> 6.0.0'
 depends 'user'
 depends 'logrotate'
